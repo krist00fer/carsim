a554a3850ac69be761d2c00237cfe2df9443b79f
Peter Bryntesson <peter_bryntesson@hotmail.com>
2018-05-28 18:20:51 +0200

    Added entity classes

---
+++ src/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Vehicle
+    {
+        public string VehicleId { get; set; }
+        public string Owner { get; set; }
+    }
+}
---
+++ src/Models/VehicleStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class VehicleStatus
+    {
+        public string VehicleId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Direction { get; set; }
+        public double Speed { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
